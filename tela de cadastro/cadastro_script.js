0e39a57138339c938fd39dc9c21218344d1a3791
pedro-rodrigues2024 <pedro.rodrigues@sptech.school>
2024-04-29 15:21:27 -0300

    tela de cadastro

--- tela de cadastro/cadastro_script.js
+++ tela de cadastro/cadastro_script.js
@@ -1,18 +1,15 @@
 
     function cadastrar(){
-        var empresa = input_empresa.value;
-        var email = input_email.value;
-        var cnpj = input_cnpj.value;
-        var senha1 = input_senha1.value;
-        var senha2 = input_senha2.value;
-
-        if(empresa === '' || cnpj === '' || email === '' || senha1 === '' || senha2 === '') {
+        var email = inpemail.value;
+        var cnpj = inpcnpj.value;
+        var senha1 = inpsenha.value;
+        var senha2 = inpsenha2.value;
+       
+        if(email == '' || cnpj == '' || senha1 == '' || senha2 == '') {
             alert("Preencha todos os campos corretamente");
-        }else {
-            if(senha1 !== senha2){
-                alert("As senhas não correspondem!");
-            } else {
-                div_mensagem.innerHTML = "Perfeito, seja bem vindo! Se quiser que nosso sistema lembre de você, marque a opção na checkbox abaixo"
-            }
+        } else if(senha1 != senha2){
+            alert("As senhas não correspondem!");
+        } else {
+            alert("Perfeito, Cadastro realizado!")
         }
     }
