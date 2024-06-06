9f28ad1542e6002502813ffa347ad026b1082aa5
pedro-rodrigues <pedro.rodrigues@sptech.school>
2024-06-06 12:50:34 -0300

    Alterei o CSS da Dashbord

---
+++ DashAlterada-MilhoTech-/Dashboard/main.js
@@ -0,0 +1,136 @@
+// Importa os módulos necessários
+// não altere!
+const serialport = require('serialport'); // Módulo para comunicação serial
+const express = require('express'); // Módulo para criar um servidor web
+const mysql = require('mysql2'); // Módulo para conectar ao MySQL
+
+// Constantes para configurações
+// não altere!
+const SERIAL_BAUD_RATE = 9600;
+const SERVIDOR_PORTA = 3300;
+
+// Habilita ou desabilita a inserção de dados no banco de dados
+// false -> nao insere
+// true -> insere
+const HABILITAR_OPERACAO_INSERIR = false;
+
+// Função para comunicação serial
+const serial = async (
+    valoresDht11Umidade,
+    valoresDht11Temperatura
+) => {
+    let poolBancoDados = ''
+
+    // Conexão com o banco de dados MySQL
+    poolBancoDados = mysql.createPool(
+        {
+            // altere!
+            // Credenciais do banco de dados
+            host: 'localhost',
+            user: 'USUARIO_DO_BANCO_LOCAL',
+            password: 'SENHA_DO_BANCO_LOCAL',
+            database: 'DATABASE_LOCAL',
+            port: 3306
+        }
+    ).promise();
+
+    // Lista as portas seriais disponíveis e procura pelo Arduino
+    const portas = await serialport.SerialPort.list();
+    const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
+    if (!portaArduino) {
+        throw new Error('O arduino não foi encontrado em nenhuma porta serial');
+    }
+
+    // Configura a porta serial com o baud rate especificado
+    const arduino = new serialport.SerialPort(
+        {
+            path: portaArduino.path,
+            baudRate: SERIAL_BAUD_RATE
+        }
+    );
+
+    // Evento quando a porta serial é aberta
+    arduino.on('open', () => {
+        console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
+    });
+
+    // Processa os dados recebidos do Arduino
+    arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
+        console.log(data);
+        const valores = data.split(';');
+        const dht11Umidade = parseFloat(valores[0]);
+        const dht11Temperatura = parseFloat(valores[1]);
+
+        // Armazena os valores dos sensores nos arrays correspondentes
+        valoresDht11Umidade.push(dht11Umidade);
+        valoresDht11Temperatura.push(dht11Temperatura);
+
+        // Insere os dados no banco de dados (se habilitado)
+        if (HABILITAR_OPERACAO_INSERIR) {
+
+            // altere!
+            // Este insert irá inserir os dados na tabela "medida"
+            await poolBancoDados.execute(
+                'INSERT INTO medida (dht11_umidade, dht11_temperatura,) VALUES (?, ?)',
+                [dht11Umidade, dht11Temperatura]
+            );
+            console.log("valores inseridos no banco: ", dht11Umidade + ", " + dht11Temperatura)
+        
+        }
+        
+    });
+
+    // Evento para lidar com erros na comunicação serial
+    arduino.on('error', (mensagem) => {
+        console.error(`Erro no arduino (Mensagem: ${mensagem}`)
+    });
+}
+
+
+// não altere!
+// Função para criar e configurar o servidor web
+const servidor = (
+    valoresDht11Umidade,
+    valoresDht11Temperatura
+) => {
+    const app = express();
+
+    // Configurações de CORS
+    app.use((request, response, next) => {
+        response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+        next();
+    });
+
+    // Inicia o servidor na porta especificada
+    app.listen(SERVIDOR_PORTA, () => {
+        console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
+    });
+
+    // Define os endpoints da API para cada tipo de sensor
+    app.get('/sensores/dht11/umidade', (_, response) => {
+        return response.json(valoresDht11Umidade);
+    });
+    app.get('/sensores/dht11/temperatura', (_, response) => {
+        return response.json(valoresDht11Temperatura);
+    });
+}
+
+// Função principal assíncrona para iniciar a comunicação serial e o servidor web
+(async () => {
+    // Arrays para armazenar os valores dos sensores
+    const valoresDht11Umidade = [];
+    const valoresDht11Temperatura = [];
+
+    // Inicia a comunicação serial
+    await serial(
+        valoresDht11Umidade,
+        valoresDht11Temperatura,
+    );
+
+    // Inicia o servidor web
+    servidor(
+        valoresDht11Umidade,
+        valoresDht11Temperatura,
+    );
+})();
