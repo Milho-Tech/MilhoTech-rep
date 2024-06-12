57a74bbc86f214e70352e112bebdd9f287a99462
Arthurhzr <arthur.rocha@sptech.school>
2024-06-12 09:42:37 -0300

    teste da dashboard humidade do silo 1

---
+++ Dash/MilhoTech-rep/public/js/alerta.js
@@ -0,0 +1,125 @@
+var alertas = [];
+
+function obterdados(EmpresaId) {
+    fetch(`/medidas/tempo-real/${EmpresaId}`)
+        .then(resposta => {
+            if (resposta.status == 200) {
+                resposta.json().then(resposta => {
+
+                    console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+
+                    alertar(resposta, EmpresaId);
+                });
+            } else {
+                console.error(`Nenhum dado encontrado para o id ${EmpresaId} ou erro na API`);
+            }
+        })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados do aquario p/ gráfico: ${error.message}`);
+        });
+
+}
+
+function alertar(resposta, EmpresaId) {
+    var temp = resposta[0].temperatura;
+
+    var grauDeAviso = '';
+
+    var limites = {
+        muito_quente: 21,
+        quente: 18,
+        ideal: 16,
+        frio: 14,
+        muito_frio: 13
+    };
+
+    var classe_temperatura = 'cor-alerta';
+
+    if (temp >= limites.muito_quente) {
+        classe_temperatura = 'cor-alerta perigo-quente';
+        grauDeAviso = 'perigo quente'
+        grauDeAvisoCor = 'cor-alerta perigo-quente'
+        exibirAlerta(temp, EmpresaId, grauDeAviso, grauDeAvisoCor)
+    }
+    else if (temp < limites.muito_quente && temp >= limites.quente) {
+        classe_temperatura = 'cor-alerta alerta-quente';
+        grauDeAviso = 'alerta quente'
+        grauDeAvisoCor = 'cor-alerta alerta-quente'
+        exibirAlerta(temp, EmpresaId, grauDeAviso, grauDeAvisoCor)
+    }
+    else if (temp < limites.quente && temp > limites.frio) {
+        classe_temperatura = 'cor-alerta ideal';
+        removerAlerta(EmpresaId);
+    }
+    else if (temp <= limites.frio && temp > limites.muito_frio) {
+        classe_temperatura = 'cor-alerta alerta-frio';
+        grauDeAviso = 'alerta frio'
+        grauDeAvisoCor = 'cor-alerta alerta-frio'
+        exibirAlerta(temp, EmpresaId, grauDeAviso, grauDeAvisoCor)
+    }
+    else if (temp <= limites.muito_frio) {
+        classe_temperatura = 'cor-alerta perigo-frio';
+        grauDeAviso = 'perigo frio'
+        grauDeAvisoCor = 'cor-alerta perigo-frio'
+        exibirAlerta(temp, EmpresaId, grauDeAviso, grauDeAvisoCor)
+    }
+
+    var card;
+
+    if (document.getElementById(`temp_aquario_${EmpresaId}`) != null) {
+        document.getElementById(`temp_aquario_${EmpresaId}`).innerHTML = temp + "°C";
+    }
+
+    if (document.getElementById(`card_${EmpresaId}`)) {
+        card = document.getElementById(`card_${EmpresaId}`)
+        card.className = classe_temperatura;
+    }
+}
+
+function exibirAlerta(temp, EmpresaId, grauDeAviso, grauDeAvisoCor) {
+    var indice = alertas.findIndex(item => item.EmpresaId == EmpresaId);
+
+    if (indice >= 0) {
+        alertas[indice] = { EmpresaId, temp, grauDeAviso, grauDeAvisoCor }
+    } else {
+        alertas.push({ EmpresaId, temp, grauDeAviso, grauDeAvisoCor });
+    }
+
+    exibirCards();
+}
+
+function removerAlerta(EmpresaId) {
+    alertas = alertas.filter(item => item.EmpresaId != EmpresaId);
+    exibirCards();
+}
+
+function exibirCards() {
+    alerta.innerHTML = '';
+
+    for (var i = 0; i < alertas.length; i++) {
+        var mensagem = alertas[i];
+        alerta.innerHTML += transformarEmDiv(mensagem);
+    }
+}
+
+function transformarEmDiv({ EmpresaId, temp, grauDeAviso, grauDeAvisoCor }) {
+
+    var descricao = JSON.parse(sessionStorage.AQUARIOS).find(item => item.id == EmpresaId).descricao;
+    return `
+    <div class="mensagem-alarme">
+        <div class="informacao">
+            <div class="${grauDeAvisoCor}">&#12644;</div> 
+            <h3>${descricao} está em estado de ${grauDeAviso}!</h3>
+            <small>Temperatura capturada: ${temp}°C.</small>   
+        </div>
+        <div class="alarme-sino"></div>
+    </div>
+    `;
+}
+
+function atualizacaoPeriodica() {
+    JSON.parse(sessionStorage.AQUARIOS).forEach(item => {
+        obterdados(item.id)
+    });
+    setTimeout(atualizacaoPeriodica, 5000);
+}
---
+++ Dash/MilhoTech-rep/src/models/medidaModel.js
@@ -0,0 +1,32 @@
+var database = require("../database/config");
+
+function buscarUltimasMedidas(fkSilo) {
+
+    var instrucaoSql = `SELECT 
+        umidade as umidade,
+                        dtRegistro,
+                        DATE_FORMAT(dtRegistro,'%H:%i:%s') as dtRegistro
+                    FROM registro where fkSilo = 1;`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal(idAquario) {
+
+    var instrucaoSql = `SELECT 
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,
+                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+                        fk_aquario 
+                        FROM medida WHERE fk_aquario = ${idAquario} 
+                    ORDER BY id DESC LIMIT 1`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    buscarUltimasMedidas,
+    buscarMedidasEmTempoReal
+}
